ff5e322216a0ce3950246ccb352612f26802977c
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-02-21 01:03:24 +0600

    with/without optional prop, index signs

--- front-end/TypeScript/app/src/05. TS_object_types.ts
+++ front-end/TypeScript/app/src/05. TS_object_types.ts
@@ -7,10 +7,32 @@
 
 
 // Type inference
-const car = {
-    type: "Toyota",
-  };
-  car.type = "Ford"; // no error
+// const car = {
+//     type: "Toyota",
+//   };
+//   car.type = "Ford"; // no error
   // car.type = 2; // Error: Type 'number' is not as
 
-  console.log(car);
+
+// Example without an optional property
+// const car: { type: string, mileage: number } = { // Error: Property 'mileage' is missing in type '{ type: string; }' but required in type '{ type: string; mileage: number; }'.
+//   type: "Toyota",
+// };
+// car.mileage = 2000;
+
+
+// Example with an optional property
+// no error on optional property, remove it and see what happens
+// const car: { type: string, mileage?: number } = {
+//   type: "Toyota"
+// };
+// car.mileage = 2000;
+
+// console.log(car);
+
+
+// Index signatures
+const nameAgeMap: { [index: string]: number } = {};
+nameAgeMap.Jack = 25; // no error
+// nameAgeMap.Mark = "Fifty"; // Error: Type 'string' is not assignable to type 'number'.
+console.log(nameAgeMap);
