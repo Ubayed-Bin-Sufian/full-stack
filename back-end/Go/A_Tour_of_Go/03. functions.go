be60d2e85b3e2ccfa0eaa2fb58b26467a0cfb40d
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-06-30 23:28:26 +0600

    Add initial functions.go file

---
+++ back-end/Go/A_Tour_of_Go/03. functions.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// // add takes two integers and returns their sum.
+// func add(x int, y int) int {
+// 	return x + y
+// }
+
+// func main() {
+// 	fmt.Println(add(53, 34))
+// }
+
+// Output: 87
