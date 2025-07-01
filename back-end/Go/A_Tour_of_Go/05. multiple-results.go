75b8dacb4680bab5c3aea68b9ad91b1650b438cd
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-01 23:33:39 +0600

    Add multiple-results.go file with commented swap function example

---
+++ back-end/Go/A_Tour_of_Go/05. multiple-results.go
@@ -0,0 +1,16 @@
+package main
+
+// import "fmt"
+
+// // swap returns its two arguments in reverse order.
+// func swap(x, y string) (string, string) {
+// 	return y, x
+// }
+
+// // Main function to demonstrate the swap function
+// func main() {
+// 	a, b := swap("hello", "world")
+// 	fmt.Println(a, b)
+// }
+
+// Output: world hello
