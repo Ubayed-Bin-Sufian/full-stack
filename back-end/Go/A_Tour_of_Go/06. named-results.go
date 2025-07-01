5e8b2e6ac9742d7ad5e74a17f3a4ca708c85b8ea
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-01 23:42:13 +0600

    Add named-results.go file with commented split function example

---
+++ back-end/Go/A_Tour_of_Go/06. named-results.go
@@ -0,0 +1,17 @@
+package main
+
+// import "fmt"
+
+// // This function takes an integer input sum. It returns two named integers, x and y.
+// // The naked return statement returns the current values of the named return variables x and y.
+// func split(sum int) (x, y int) {
+// 	x = sum * 4 / 9
+// 	y = sum - x
+// 	return
+// }
+
+// func main() {
+// 	fmt.Println(split(17))
+// }
+
+// Output: 7 10
