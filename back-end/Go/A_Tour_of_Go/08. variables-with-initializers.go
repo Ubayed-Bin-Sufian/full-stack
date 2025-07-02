28f43f45208279aa3191abefbfd484d498bb597b
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-03 01:08:52 +0600

    Lines commented

--- back-end/Go/A_Tour_of_Go/08. variables-with-initializers.go
+++ back-end/Go/A_Tour_of_Go/08. variables-with-initializers.go
@@ -1,14 +1,14 @@
 package main
 
-import "fmt"
+// import "fmt"
 
-// Variables with initializers
-// If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
-var i, j int = 1, 2
+// // Variables with initializers
+// // If an initializer is present, the type can be omitted; the variable will take the type of the initializer.
+// var i, j int = 1, 2
 
-func main() {
-	var c, python, java = true, false, "no!"
-	fmt.Println(i, j, c, python, java)
-}
+// func main() {
+// 	var c, python, java = true, false, "no!"
+// 	fmt.Println(i, j, c, python, java)
+// }
 
 // Output: 1 2 true false no!
