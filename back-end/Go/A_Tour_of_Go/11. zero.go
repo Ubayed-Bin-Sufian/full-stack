ef4a241b911cfd44b3307434a11a31de5503e15d
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-04 01:57:36 +0600

    Add initial zero values example in Go

---
+++ back-end/Go/A_Tour_of_Go/11. zero.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// // Zero values are the default values for variables in Go when they are declared but not initialized.
+// func main() {
+// 	var i int
+// 	var f float64
+// 	var b bool
+// 	var s string
+// 	fmt.Printf("%v %v %v %q\n", i, f, b, s)
+// }
+
+// Output: 0 0 false ""
