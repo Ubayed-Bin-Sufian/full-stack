e82945c00a757863bbab9a911ae76f53f5f1a6de
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-03 23:53:30 +0600

    Add basic types example in Go

---
+++ back-end/Go/A_Tour_of_Go/10. basic-types.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/cmplx"
+)
+
+// This is a basic types example in Go
+var (
+	ToBe   bool       = false
+	MaxInt uint64     = 1<<64 - 1
+	z      complex128 = cmplx.Sqrt(-5 + 12i)
+)
+
+// The main function prints the types and values of the variables defined above
+func main() {
+	fmt.Printf("Type: %T Value: %v\n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v\n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v\n", z, z)
+}
+
+// The output of this program will be:
+// Type: bool Value: false
+// Type: uint64 Value: 18446744073709551615
+// Type: complex128 Value: (3+4i)
