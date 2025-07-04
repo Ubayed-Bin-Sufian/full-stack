da4cb1f3a9ac05f92baefa0b3e8c232c61d154bf
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-05 05:01:21 +0600

    Add type inference example in Go

---
+++ back-end/Go/A_Tour_of_Go/13. type-inference.go
@@ -0,0 +1,18 @@
+package main
+
+// import "fmt"
+
+// // This program demonstrates type inference in Go.
+// func main() {
+// 	v := 15         // change me!
+// 	w := 15.2       // change me!
+// 	x := 15.1 + 15i // change me!
+// 	fmt.Printf("v is of type %T\n", v)
+// 	fmt.Printf("w is of type %T\n", w)
+// 	fmt.Printf("x is of type %T\n", x)
+// }
+
+// Output:
+// v is of type int
+// w is of type float64
+// x is of type complex128
