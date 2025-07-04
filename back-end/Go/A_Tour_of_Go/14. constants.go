fe05ce45c0ba6650f63dcb13c0e0ca1c3ef7a2cf
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-05 05:06:29 +0600

    Add constants example in Go

---
+++ back-end/Go/A_Tour_of_Go/14. constants.go
@@ -0,0 +1,20 @@
+package main
+
+// import "fmt"
+
+// const Pi = 3.14
+
+// // This program demonstrates the use of constants in Go.
+// func main() {
+// 	const World = "বিশ্ব"
+// 	fmt.Println("Hello", World)
+// 	fmt.Println("Happy", Pi, "Day")
+
+// 	const Truth = true
+// 	fmt.Println("Go rules?", Truth)
+// }
+
+// Output:
+// Hello বিশ্ব
+// Happy 3.14 Day
+// Go rules? true
