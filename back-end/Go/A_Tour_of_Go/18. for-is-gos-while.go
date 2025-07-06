fde10fb8a4d80bb0e5c2c25b8f12acc5a14d004f
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-06 06:18:15 +0600

    Example without semicolons in for loop

---
+++ back-end/Go/A_Tour_of_Go/18. for-is-gos-while.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// // The semicolon is optional in Go.
+// func main() {
+// 	sum := 1
+// 	for sum < 1000 {
+// 		sum += sum
+// 	}
+// 	fmt.Println(sum)
+// }
+
+// Output: 1024
