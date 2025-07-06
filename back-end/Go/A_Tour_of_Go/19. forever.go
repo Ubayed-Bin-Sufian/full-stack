f30d9e65e0cf123201a63fe7bfc02ce6e351a394
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-06 06:21:26 +0600

    Example of for loop to create an indefinite loop

---
+++ back-end/Go/A_Tour_of_Go/19. forever.go
@@ -0,0 +1,9 @@
+package main
+
+// The for loop can be used to create an infinite loop in Go.
+// func main() {
+// 	for {
+// 	}
+// }
+
+// Output: This program will run indefinitely until manually stopped.
