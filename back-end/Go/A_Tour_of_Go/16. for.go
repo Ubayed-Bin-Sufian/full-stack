545410c569859568cf2b408ae0f6edaf3e8628c1
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-06 06:05:23 +0600

    Add example of for loop in Go

---
+++ back-end/Go/A_Tour_of_Go/16. for.go
@@ -0,0 +1,14 @@
+package main
+
+// import "fmt"
+
+// // The for loop is the only loop in Go.
+// func main() {
+// 	sum := 0
+// 	for i := 0; i < 10; i++ {
+// 		sum += i
+// 	}
+// 	fmt.Println(sum)
+// }
+
+// Output: 45
