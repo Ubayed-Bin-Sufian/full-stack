3a6b65222a972500f69eae0979d408765c9d44f5
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-07 04:34:51 +0600

    Example with if condition

---
+++ back-end/Go/A_Tour_of_Go/20. if.go
@@ -0,0 +1,20 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// // The sqrt function calculates the square root of a number.
+// func sqrt(x float64) string {
+// 	if x < 0 {
+// 		return sqrt(-x) + "i"
+// 	}
+// 	return fmt.Sprint(math.Sqrt(x))
+// }
+
+// func main() {
+// 	fmt.Println(sqrt(2), sqrt(-4))
+// }
+
+// Output: 1.4142135623730951 2i
