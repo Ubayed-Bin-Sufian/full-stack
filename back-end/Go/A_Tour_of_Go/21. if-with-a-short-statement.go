6d808352083885e6f1b5e04d3f859ec445129d5c
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-07 16:17:34 +0600

    Example of if with a short statement

---
+++ back-end/Go/A_Tour_of_Go/21. if-with-a-short-statement.go
@@ -0,0 +1,23 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"math"
+// )
+
+// // The pow function calculates x raised to the power of n, but limits the result to lim.
+// func pow(x, n, lim float64) float64 {
+// 	if v := math.Pow(x, n); v < lim {
+// 		return v
+// 	}
+// 	return lim
+// }
+
+// func main() {
+// 	fmt.Println(
+// 		pow(3, 2, 10),
+// 		pow(3, 3, 20),
+// 	)
+// }
+
+// Output: 9 20
