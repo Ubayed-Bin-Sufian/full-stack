91009647e83e4cd4c199f351c4d98590d4c4c559
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-10 23:56:57 +0600

    Example of switch statement

---
+++ back-end/Go/A_Tour_of_Go/24. switch.go
@@ -0,0 +1,23 @@
+package main
+
+// import (
+// 	"fmt"
+// 	"runtime"
+// )
+
+// // A switch statement is a shorter way to write a sequence of if - else statements.
+// func main() {
+// 	fmt.Print("Go runs on ")
+// 	switch os := runtime.GOOS; os {
+// 	case "darwin":
+// 		fmt.Println("macOS.")
+// 	case "linux":
+// 		fmt.Println("Linux.")
+// 	default:
+// 		// freebsd, openbsd,
+// 		// plan9, windows...
+// 		fmt.Printf("%s.\n", os)
+// 	}
+// }
+
+// Output: Go runs on windows.
