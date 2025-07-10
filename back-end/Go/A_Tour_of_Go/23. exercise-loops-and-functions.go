8e4505e5c056ab9ea3ff938dccf4404815499671
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-11 03:54:31 +0600

    Commented

--- back-end/Go/A_Tour_of_Go/23. exercise-loops-and-functions.go
+++ back-end/Go/A_Tour_of_Go/23. exercise-loops-and-functions.go
@@ -1,23 +1,23 @@
 package main
 
-import (
-	"fmt"
-)
+// import (
+// 	"fmt"
+// )
 
-func Sqrt(x float64) float64 {
-	z := 1.0
-	count := 0
+// func Sqrt(x float64) float64 {
+// 	z := 1.0
+// 	count := 0
 
-	for count < 10 {
-		if z < x {
-			z -= (z*z - x) / (2 * z) // Newton's method for square root approximation
-			// fmt.Println(z)  // TO check how the value comes nearest to square root
-		}
-		count++
-	}
-	return z
-}
+// 	for count < 10 {
+// 		if z < x {
+// 			z -= (z*z - x) / (2 * z) // Newton's method for square root approximation
+// 			// fmt.Println(z)  // TO check how the value comes nearest to square root
+// 		}
+// 		count++
+// 	}
+// 	return z
+// }
 
-func main() {
-	fmt.Println(Sqrt(1))
-}
+// func main() {
+// 	fmt.Println(Sqrt(1))
+// }
