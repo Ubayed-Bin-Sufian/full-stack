a11d71a008978a160d976567b11833fe483cfb65
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-15 05:58:10 +0600

    Example of pointers

---
+++ back-end/Go/A_Tour_of_Go/29. pointers.go
@@ -0,0 +1,22 @@
+package main
+
+// import "fmt"
+
+// // Pointers are a way to reference the memory address of a variable.
+// func main() {
+// 	i, j := 42, 2701
+
+// 	p := &i         // point to i
+// 	fmt.Println(*p) // read i through the pointer
+// 	*p = 21         // set i through the pointer
+// 	fmt.Println(i)  // see the new value of i
+
+// 	p = &j         // point to j
+// 	*p = *p / 37   // divide j through the pointer
+// 	fmt.Println(j) // see the new value of j
+// }
+
+// Output:
+// 42
+// 21
+// 73
