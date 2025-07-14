8ecd874f96721d1799da45b574c4d67a01a73267
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-14 07:03:20 +0600

    Example of stacking defers

---
+++ back-end/Go/A_Tour_of_Go/28. defer-multi.go
@@ -0,0 +1,28 @@
+package main
+
+// import "fmt"
+
+// // Deferred function calls are pushed onto a stack. When a function returns, its deferred calls are executed in last-in-first-out order.
+// func main() {
+// 	fmt.Println("counting")
+
+// 	for i := 0; i < 10; i++ {
+// 		defer fmt.Println(i)
+// 	}
+
+// 	fmt.Println("done")
+// }
+
+// Output:
+// counting
+// done
+// 9
+// 8
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
