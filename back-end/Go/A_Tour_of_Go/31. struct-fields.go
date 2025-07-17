e20085d0562d3670649d9231c597ee5c417f756b
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-17 08:58:15 +0600

    Example of struts fields

---
+++ back-end/Go/A_Tour_of_Go/31. struct-fields.go
@@ -0,0 +1,23 @@
+package main
+
+// import "fmt"
+
+// type Vertex struct {
+// 	X int
+// 	Y float64
+// }
+
+// // Struct fields are accessed using a dot.
+// func main() {
+// 	v := Vertex{1, 2}
+// 	v.X = 18
+// 	v.Y = 20.6
+// 	fmt.Println(v.X)
+// 	fmt.Println(v.Y)
+// 	fmt.Println(v)
+// }
+
+// Output:
+// 18
+// 20.6
+// {18 20.6}
