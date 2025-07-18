ad9edcdd683dd08ce685b5066d0bc29349f3c2a9
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-18 08:14:48 +0600

    Example of struts pointers

---
+++ back-end/Go/A_Tour_of_Go/32. struct-pointers.go
@@ -0,0 +1,19 @@
+package main
+
+// import "fmt"
+
+// type Vertex struct {
+// 	X int
+// 	Y int
+// }
+
+// // Struct fields can be accessed through a struct pointer.
+// func main() {
+// 	v := Vertex{737, 233637}
+// 	p := &v
+// 	p.Y = 5e10
+// 	fmt.Println(v)
+// }
+
+// Output:
+// {737 50000000000}
