5f2e9e31b34c6dd59126206aac8649363c857dd6
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-20 18:07:52 +0600

    Example of array

---
+++ back-end/Go/A_Tour_of_Go/34. array.go
@@ -0,0 +1,20 @@
+package main
+
+// import "fmt"
+
+// // This program demonstrates the use of arrays in Go.
+// // The type [n]T is an array of n values of type T.
+// func main() {
+// 	var a [2]string
+// 	a[0] = "Hello"
+// 	a[1] = "World"
+// 	fmt.Println(a[0], a[1])
+// 	fmt.Println(a)
+
+// 	primes := [6]int{2, 3, 5, 7, 11, 13}
+// 	fmt.Println(primes)
+// }
+
+// Output: Hello World
+// [Hello World]
+// [2 3 5 7 11 13]
