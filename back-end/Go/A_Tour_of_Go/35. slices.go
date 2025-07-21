d0ceb3d787eced2aa2707f1cf0a31b460771f9a1
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-07-21 23:55:31 +0600

    Example of slices

---
+++ back-end/Go/A_Tour_of_Go/35. slices.go
@@ -0,0 +1,15 @@
+package main
+
+// import "fmt"
+
+// // This program demonstrates how to create a slice from an array in Go.
+// // The type []T is a slice with elements of type T.
+// // This selects a half-open range which includes the first element, but excludes the last one.
+// func main() {
+// 	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+// 	var s []int = primes[1:4]
+// 	fmt.Println(s)
+// }
+
+// Output: [3 5 7]
