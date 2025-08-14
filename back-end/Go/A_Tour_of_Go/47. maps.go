4cc82d9796b4f75230da1985b3e03da7ad524ed2
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-08-15 00:48:48 +0600

    add detail explanation and output

--- back-end/Go/A_Tour_of_Go/47. maps.go
+++ back-end/Go/A_Tour_of_Go/47. maps.go
@@ -6,12 +6,29 @@ package main
 // 	Lat, Long float64
 // }
 
+// // Declare a map (but it's nil at first)
+// // A nil map: 
+// // Has no memory space allocated.
+// // Cannot add keys until you make one.
+// // m → nil (no storage, no keys allowed)
 // var m map[string]Vertex
 
 // func main() {
+// 	// Map is created and initialized but is empty
+// 	// m → {}
 // 	m = make(map[string]Vertex)
+
+// 	// Adding the key-value pair
+// 	// Key: Bell Labs 
+// 	// Value: Vertex{40.68433, -74.39967}
+// 	// m → {
+// 	//     "Bell Labs": Vertex{Lat: 40.68433, Long: -74.39967}
+// 	// }
 // 	m["Bell Labs"] = Vertex{
 // 		40.68433, -74.39967,
 // 	}
-// 	fmt.Println(m["Bell Labs"])
+// 	fmt.Println(m["Bell Labs"])  // fetches the value for the key "Bell Labs".
 // }
+
+// Output
+// {40.68433 -74.39967}
