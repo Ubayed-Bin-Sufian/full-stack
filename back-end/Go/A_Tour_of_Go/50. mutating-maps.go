f4e41490e4d3b817cfaf3aed5a99b4b8b44193ab
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-08-20 22:59:53 +0600

    add explanation and output

--- back-end/Go/A_Tour_of_Go/50. mutating-maps.go
+++ back-end/Go/A_Tour_of_Go/50. mutating-maps.go
@@ -14,6 +14,16 @@ package main
 // 	delete(m, "Answer")
 // 	fmt.Println("The value:", m["Answer"])
 
+// 	// When you read m[key] with two results, Go returns:
+// 	// v: the value (or the zero value if missing)
+// 	// ok: a bool that’s true if the key exists, false otherwise
+// 	// Since we deleted the key, ok is false and v is 0.
 // 	v, ok := m["Answer"]
 // 	fmt.Println("The value:", v, "Present?", ok)
 // }
+
+// Ouput
+// The value: 42
+// The value: 48
+// The value: 0
+// The value: 0 Present? false
