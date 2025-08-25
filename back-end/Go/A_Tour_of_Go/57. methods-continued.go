abae01e027716b91773b1ab2362d6e9fa4ca0a79
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-08-25 09:10:25 +0600

    Comment

--- back-end/Go/A_Tour_of_Go/57. methods-continued.go
+++ back-end/Go/A_Tour_of_Go/57. methods-continued.go
@@ -1,27 +1,27 @@
 package main
 
-import (
-	"fmt"
-	"math"
-)
+// import (
+// 	"fmt"
+// 	"math"
+// )
 
-type MyFloat float64
+// type MyFloat float64
 
-// If you want to add methods to an existing type (like int, float64, string),
-// you must first create a new named type based on it, and then define methods on that.
-// You can only declare a method with a receiver 
-// whose type is defined in the same package as the method.
-func (f MyFloat) Abs() float64 {
-	if f < 0 {
-		return float64(-f)
-	}
-	return float64(f)
-}
+// // If you want to add methods to an existing type (like int, float64, string),
+// // you must first create a new named type based on it, and then define methods on that.
+// // You can only declare a method with a receiver 
+// // whose type is defined in the same package as the method.
+// func (f MyFloat) Abs() float64 {
+// 	if f < 0 {
+// 		return float64(-f)
+// 	}
+// 	return float64(f)
+// }
 
-func main() {
-	f := MyFloat(-math.Sqrt2)
-	fmt.Println(f.Abs())
-}
+// func main() {
+// 	f := MyFloat(-math.Sqrt2)
+// 	fmt.Println(f.Abs())
+// }
 
 // Output:
 // 1.4142135623730951
