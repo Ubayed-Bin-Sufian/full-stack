209419f17bd160843c4dae933ffc1055f8c6995e
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-08-25 09:42:16 +0600

    Explanation and output added

--- back-end/Go/A_Tour_of_Go/58. methods-pointers.go
+++ back-end/Go/A_Tour_of_Go/58. methods-pointers.go
@@ -9,10 +9,16 @@ package main
 // 	X, Y float64
 // }
 
+// // This has a value receiver (v Vertex).
+// // That means the method gets a copy of the Vertex.
+// // It does not change the original vertex.
 // func (v Vertex) Abs() float64 {
 // 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 // }
 
+// // This has a pointer receiver (v *Vertex).
+// // That means the method can modify the original struct.
+// // It scales (multiplies) both coordinates by f.
 // func (v *Vertex) Scale(f float64) {
 // 	v.X = v.X * f
 // 	v.Y = v.Y * f
@@ -23,3 +29,6 @@ package main
 // 	v.Scale(10)
 // 	fmt.Println(v.Abs())
 // }
+
+// Output:
+// 50
