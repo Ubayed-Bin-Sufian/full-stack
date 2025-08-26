4001c41c84c79d5130dd2baf9ef519f5d8f04bd1
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-08-26 07:51:34 +0600

    Explanation and output added

--- back-end/Go/A_Tour_of_Go/59. methods-pointers-explained.go
+++ back-end/Go/A_Tour_of_Go/59. methods-pointers-explained.go
@@ -9,10 +9,15 @@ package main
 // 	X, Y float64
 // }
 
+// // Abs is a function (not a method) that calculates
+// // the Euclidean distance of a Vertex from the origin.
 // func Abs(v Vertex) float64 {
 // 	return math.Sqrt(v.X*v.X + v.Y*v.Y)
 // }
 
+// // Scale takes a pointer to a Vertex and scales its coordinates
+// // by multiplying them with the factor f.
+// // Because we pass a pointer, the original Vertex gets modified.
 // func Scale(v *Vertex, f float64) {
 // 	v.X = v.X * f
 // 	v.Y = v.Y * f
@@ -23,3 +28,6 @@ package main
 // 	Scale(&v, 10)
 // 	fmt.Println(Abs(v))
 // }
+
+// Output:
+// 50
