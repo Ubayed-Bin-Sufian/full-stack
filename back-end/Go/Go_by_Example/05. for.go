7fc686f24fbb0510781a2b98f6ecea9a9c016b54
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-09-26 23:07:53 +0600

    for

---
+++ back-end/Go/Go_by_Example/05. for.go
@@ -0,0 +1,53 @@
+package main
+
+// import "fmt"
+
+// func main() {
+
+// 	// While-style loop (single condition)
+//     i := 1
+//     for i <= 3 {
+//         fmt.Println(i)
+//         i = i + 1
+//     }
+
+// 	// Classic for loop (init; condition; after)
+//     for j := 0; j < 3; j++ {
+//         fmt.Println(j)
+//     }
+
+// 	// Range over an integer from 0 to N-1
+//     for i := range 3 {
+//         fmt.Println("range", i)
+//     }
+
+// 	// for {} means infinite loop
+// 	// Infinite loop with break
+//     for {
+//         fmt.Println("loop")
+//         break
+//     }
+
+// 	// continue skips the current iteration when n is even.
+//     for n := range 6 {
+//         if n%2 == 0 {
+//             continue
+//         }
+//         fmt.Println(n)
+//     }
+// }
+
+// Output:
+// 1
+// 2      
+// 3      
+// 0      
+// 1      
+// 2      
+// range 0
+// range 1
+// range 2
+// loop   
+// 1
+// 3
+// 5
