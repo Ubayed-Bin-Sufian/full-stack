4007a82ecc22d2c2f3e1bd87034846c604df99a6
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2025-09-28 15:11:32 +0600

    switch

---
+++ back-end/Go/Go_by_Example/07. switch.go
@@ -0,0 +1,65 @@
+package main
+
+// import (
+//     "fmt"
+//     "time"
+// )
+
+// func main() {
+
+// 	// basic switch
+//     i := 2
+//     fmt.Print("Write ", i, " as ")
+//     switch i {
+//     case 1:
+//         fmt.Println("one")
+//     case 2:
+//         fmt.Println("two")
+//     case 3:
+//         fmt.Println("three")
+//     }
+
+// 	// commas to separate multiple expressions in the same case statement
+//     switch time.Now().Weekday() {
+//     case time.Friday, time.Saturday:
+//         fmt.Println("It's the weekend")
+//     default:
+//         fmt.Println("It's a weekday")
+//     }
+
+// 	// switch without an expression
+// 	// same as switch true {}
+// 	// The first condition that evaluates to true will execute
+// 	// cases are boolean expressions
+//     t := time.Now()
+//     switch {
+//     case t.Hour() < 12:
+//         fmt.Println("It's before noon")
+//     default:
+//         fmt.Println("It's afternoon")
+//     }
+
+// 	// A type switch compares types instead of values. 
+// 	// This can be used to discover the type of an interface value.
+//     whatAmI := func(i interface{}) {
+//         switch t := i.(type) {
+//         case bool:
+//             fmt.Println("I'm a bool")
+//         case int:
+//             fmt.Println("I'm an int")
+//         default:
+//             fmt.Printf("Don't know type %T\n", t)
+//         }
+//     }
+//     whatAmI(true)
+//     whatAmI(1)
+//     whatAmI("hey")
+// }
+
+// Output:
+// Write 2 as two
+// It's a weekday
+// It's afternoon
+// I'm a bool
+// I'm an int
+// Don't know type string
