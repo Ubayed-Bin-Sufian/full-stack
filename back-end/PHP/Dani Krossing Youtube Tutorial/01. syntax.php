596123b88dff282a2e7fda4c3438c0281ea966b1
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2026-01-03 21:45:15 +0600

    Refactor PHP code examples for clarity and consistency

--- back-end/PHP/Dani Krossing Youtube Tutorial/01. syntax.php
+++ back-end/PHP/Dani Krossing Youtube Tutorial/01. syntax.php
@@ -7,14 +7,17 @@
     <?php echo 'This is a line of PHP code embedded in HTML.'; ?>
 </body>
 
+<?php 
 // Use semicolons to terminate statements in PHP
 // echo is used to output strings
-<?php echo 'This is a statement terminated with a semicolon.'; ?>
+echo 'This is a statement terminated with a semicolon.'; 
+?>
 
 
+<?php 
 // Example #1 PHP Opening and Closing Tags
 
-<?php echo 'if you want to serve PHP code in XHTML or XML documents,
+echo 'if you want to serve PHP code in XHTML or XML documents,
             use these tags'; ?>
 
 You can use the short echo tag to <?= 'print this string' ?>.
@@ -29,18 +32,64 @@ It's equivalent to <?php echo 'print this string' ?>.
             'errors'; ?>
 
 
+<?php
 // Example #2 PHP Code Only File
 
 echo "Hello world\n";
 
 // ... more code
-
 echo "Last statement\n";
 
 // the script ends here with no PHP closing tag
+?>
 
-// Example #3 Embedding PHP in HTML
+
+<?php // Example #3 Embedding PHP in HTML ?>
 
 <p>This is going to be ignored by PHP and displayed by the browser.</p>
 <?php echo 'While this is going to be parsed.'; ?>
 <p>This will also be ignored by PHP and displayed by the browser.</p>
+
+
+<?php // Example #4 Advanced escaping using conditions ?>
+
+<?php if ($expression == true): ?>
+  This will show if the expression is true.
+<?php else: ?>
+  Otherwise this will show.
+<?php endif; ?>
+
+
+<?php // Example #5 Example showing the closing tag encompassing the trailing newline ?>
+
+<?php echo "Some text"; ?>
+No newline
+<?= "But newline now" ?>
+
+
+<?php // Example #6 Examples of entering and exiting the PHP parser ?>
+
+<?php
+    echo "This is a test\n";
+?>
+
+<?php echo "This is a test\n" ?>
+
+<?php echo "We omitted the last closing tag\n"; ?>
+
+
+<?php // Example #7 Comments ?>
+
+<?php
+    echo "This is a test\n"; // This is a one-line c++ style comment
+    /* This is a multi line comment
+       yet another line of comment */
+    echo "This is yet another test\n";
+    echo "One Final Test\n"; # This is a one-line shell-style comment
+?>
+
+
+<?php // Example #8 One Line Comments in HTML ?>
+
+<h1>This is an <?php # echo 'simple';?> example</h1>
+<p>The header above will say 'This is an  example'.</p>
