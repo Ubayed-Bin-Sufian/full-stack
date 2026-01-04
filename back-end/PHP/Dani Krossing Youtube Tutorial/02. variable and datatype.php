f2d0b5cee8ea8fd46c1a0e4ca069a9ce96554775
Ubayed Bin Sufian <ubayedbinsufian@gmail.com>
2026-01-04 21:28:13 +0600

    Enhance PHP variable and datatype example with additional explanations and formatting

--- back-end/PHP/Dani Krossing Youtube Tutorial/02. variable and datatype.php
+++ back-end/PHP/Dani Krossing Youtube Tutorial/02. variable and datatype.php
@@ -1,3 +1,29 @@
 <?php 
 $myName = "Ubayed Bin Sufian";
-echo "My name is " . $myName . "\n";
+echo "My name is " . $myName . "<br>";
+echo "Welcome to PHP programming!<br>";
+?>
+
+<?php
+// Example #1 Valid variable names
+
+$var = 'Bob';
+$Var = 'Joe';
+echo "$var, $Var". "<br>";      // outputs "Bob, Joe"
+
+$_4site = 'not yet';    // valid; starts with an underscore
+$täyte = 'mansikka';    // valid; 'ä' is (Extended) ASCII 228.
+
+
+// Example #2 Invalid variable names
+
+// $4site = 'not yet';     // invalid; starts with a number
+
+
+// Example #3 Accessing obscure variable names
+
+${'invalid-name'} = 'bar';
+$name = 'invalid-name';
+echo ${'invalid-name'}, " ", $$name;
+
+?>
